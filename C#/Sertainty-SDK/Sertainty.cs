cfc49a7e834b7b0dc803aff52bc7790bcf67e4e4
arunwij <aruna@deepdatainsight.com>
2019-08-08 17:40:48 +0530

    C# repository

---
+++ C#/Sertainty-SDK/Sertainty.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SertaintySDK
+{
+    static class Sertainty
+    {
+        public static bool HasError(IntPtr handle)
+        {
+            return SertaintyCore.uxpsys_hasError(handle);
+        }
+
+        public static IntPtr GetErrorMessage(IntPtr handle)
+        {
+            return SertaintyCore.uxpsys_getErrorMessage(handle);
+        }
+
+        public static void SetLogFile(string prefix, string version)
+        {
+            SertaintyCore.uxpsys_setLogFile(prefix, version);
+        }
+
+        public static void FileReadAll(CallStatus status, string filespec, ByteArray outbuf)
+        {
+            SertaintyCore.uxpsys_fileReadAll(status, filespec, outbuf);
+        }
+
+        public static long InitializeLibrary(ByteArray buffer, long argc, string[] argv, string licenseFile, string appKey, string prefix, string version)
+        {
+            return SertaintyCore.uxpsys_initLibrary(buffer, argc, argv, licenseFile, appKey, prefix, version);
+        }
+
+        public class CallStatus
+        {
+            public CallStatus()
+            {
+                _handle = SertaintyCore.uxpsys_newCallStatusHandle();
+            }
+
+            public bool HasError
+            {
+                get
+                {
+                    return Sertainty.HasError(_handle);
+                }
+            }
+
+            public string ErrorMessage
+            {
+                get
+                {
+                    ByteArray errorBa = new ByteArray(Sertainty.GetErrorMessage(_handle));
+                    return errorBa.ToString();
+                }
+            }
+
+            public static implicit operator IntPtr(CallStatus cs)
+            {
+                return cs._handle;
+            }
+
+            ~CallStatus()
+            {
+                SertaintyCore.uxpsys_freeCallStatusHandle(_handle);
+            }
+
+            private IntPtr _handle;
+        }
+    }
+
+    class ByteArray
+    {
+        public ByteArray()
+        {
+            _handle = SertaintyCore.uxpba_newHandle();
+        }
+
+        public ByteArray(IntPtr srcHandle)
+        {
+            _handle = srcHandle;
+        }
+
+        public long Size
+        {
+            get
+            {
+                return SertaintyCore.uxpba_getSize(_handle);
+            }
+        }
+
+        public void Clear()
+        {
+            SertaintyCore.uxpba_clearData(_handle);
+        }
+
+        public byte[] GetBytes(int len)
+        {
+            IntPtr ptr = SertaintyCore.uxpba_getData(_handle);
+            if (ptr == IntPtr.Zero)
+                return new byte[0];
+            if (len == 0)
+                return new byte[0];
+            byte[] array = new byte[len];
+            Marshal.Copy(ptr, array, 0, len);
+            return array;
+        }
+
+        public byte[] GetAllBytes()
+        {
+            return GetBytes((int)this.Size);
+        }
+
+        public void SetData(string data)
+        {
+            SertaintyCore.uxpba_setData(_handle, data, data.Length);
+        }
+
+        public override string ToString()
+        {
+            IntPtr ptr = SertaintyCore.uxpba_getData(_handle);
+            if (ptr == IntPtr.Zero)
+                return "";
+            int len = 0;
+            while (Marshal.ReadByte(ptr, len) != 0)
+                len++;
+            if (len == 0)
+                return "";
+            byte[] array = new byte[len];
+            Marshal.Copy(ptr, array, 0, len);
+
+            return Encoding.UTF8.GetString(array);
+        }
+
+        public static implicit operator IntPtr(ByteArray byteArray)
+        {
+            return byteArray._handle;
+        }
+
+        ~ByteArray()
+        {
+            SertaintyCore.uxpba_freeHandle(_handle);
+        }
+
+        private IntPtr _handle;
+    }
+
+    class UxpFile
+    {
+        public UxpFile()
+        {
+            _handle = SertaintyCore.uxpfile_newHandle();
+        }
+
+        public void OpenNewFile(string data, string governance, long govtype, long mods, long flags)
+        {
+            SertaintyCore.uxpfile_openNewFile(_handle, data, governance, govtype, mods, flags);
+        }
+
+        public void AddVirtualFile(string virName, string filespec, long pageSize, long cacheSize, long mods)
+        {
+            SertaintyCore.uxpfile_addVirtualFromFile(_handle, virName, filespec, pageSize, cacheSize, mods);
+        }
+
+        public VirtualFile OpenVirtualFile(string fileSpec, Mode mode)
+        {
+            return new VirtualFile(this, fileSpec, mode);
+        }
+
+        public bool CompareExternalFle(string vf_name, string ext_file_name)
+        {
+            return SertaintyCore.uxpfile_compareExternalFile(_handle, vf_name, ext_file_name);
+        }
+
+        public void Close()
+        {
+            SertaintyCore.uxpfile_close(_handle);
+        }
+
+        public void Open(string source, Mode mode)
+        {
+            SertaintyCore.uxpfile_openFile(_handle, source, mode);
+        }
+
+        public AuthorizationStatus Authenticate()
+        {
+            return SertaintyCore.uxpfile_authenticate(_handle);
+        }
+
+        public int ChallengeCount
+        {
+            get
+            {
+                return SertaintyCore.uxpfile_getChallengeCount(_handle);
+            }
+        }
+
+        public Challenge GetChallenge(int ch_idx)
+        {
+            IntPtr ch_handle = SertaintyCore.uxpfile_getChallenge(_handle, ch_idx);
+            return Challenge.FromHandle(ch_handle);
+        }
+
+        public void AddResponse(Challenge ch)
+        {
+            SertaintyCore.uxpfile_addResponse(_handle, ch);
+        }
+
+        public bool HasError
+        {
+            get
+            {
+                return Sertainty.HasError(_handle);
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                ByteArray errorBa = new ByteArray(Sertainty.GetErrorMessage(_handle));
+                return errorBa.ToString();
+            }
+        }
+
+
+        public static implicit operator IntPtr(UxpFile uxp)
+        {
+            return uxp._handle;
+        }
+
+        ~UxpFile()
+        {
+            SertaintyCore.uxpfile_freeHandle(_handle);
+        }
+
+        private IntPtr _handle;
+    }
+
+    class Challenge
+    {
+        private Challenge(IntPtr ch_handle)
+        {
+            _handle = ch_handle;
+        }
+
+        public static Challenge FromHandle(IntPtr ch_handle)
+        {
+            return new Challenge(ch_handle);
+        }
+
+        public void StartTimer()
+        {
+            SertaintyCore.uxpch_startTimer(_handle);
+        }
+
+        public void EndTimer()
+        {
+            SertaintyCore.uxpch_endTimer(_handle);
+        }
+
+        public ByteArray GetPrompt()
+        {
+            ByteArray prompt = new ByteArray();
+            SertaintyCore.uxpch_getPrompt(_handle, prompt);
+            return prompt;
+        }
+
+        public void SetValue(string value)
+        {
+            SertaintyCore.uxpch_setValueString(_handle, value);
+        }
+
+        public static implicit operator IntPtr(Challenge ch)
+        {
+            return ch._handle;
+        }
+
+        ~Challenge()
+        {
+            SertaintyCore.uxpch_freeHandle(_handle);
+        }
+
+        private IntPtr _handle;
+    }
+
+    class VirtualFile
+    {
+        public VirtualFile(UxpFile uxp, string fileSpec, Mode mode)
+        {
+            _uxp = uxp;
+            _handle = SertaintyCore.uxpfile_openVirtualFile(uxp, fileSpec, mode);
+        }
+
+        public long Read(ByteArray buffer, long max_size)
+        {
+            return SertaintyCore.uxpfile_readVirtualFile(_uxp, this, buffer, max_size);
+        }
+
+        public void Close()
+        {
+            SertaintyCore.uxpfile_closeVirtualFile(_uxp, this);
+        }
+
+        public static implicit operator IntPtr(VirtualFile vf)
+        {
+            return vf._handle;
+        }
+
+        ~VirtualFile()
+        {
+
+        }
+
+        private IntPtr _handle;
+        private UxpFile _uxp;
+    }
+
+    static class Id
+    {
+        public static void PublishIdToFile(Sertainty.CallStatus status, string id, string doc, long mods)
+        {
+            SertaintyCore.uxpid_publishToFile(status, id, doc, mods);
+        }
+    }
+}
---
+++ C#/Sertainty-SDK/SertaintyCore.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SertaintySDK
+{
+    struct SertaintyCore2
+    {
+        public const string dll = "SertaintyCore2.dll";
+    }
+
+    enum Modifiers
+    {
+        Replace = 0x00001,
+        Merge = 0x00002,
+        Recurse = 0x00004,
+        Compress = 0x00008,
+        IncHidden = 0x00010,
+        Shred = 0x00020,
+        IncRealities = 0x00040,
+        NoOptimize = 0x00080,
+        DeleteSrc = 0x00100,
+        MinSize = 0x00200,
+        Reclaim = 0x00400,
+        RecurseVirtual = 0x00800,
+        Create = 0x01000,
+        Load = 0x02000,
+        ReadWrite = 0x04000,
+        Protect = 0x08000,
+        IncScript = 0x10000,
+        Formatted = 0x20000,
+        TokenReplace = 0x40000
+    }
+
+    enum Mode
+    {
+        ReadOnly = 1,
+        WriteOnly = 2,
+        ReadWrite = 3
+    }
+
+    enum AuthorizationStatus
+    {
+        NotAuthorized = 0x00001,  /*!< Access to UXP is not authorized */
+        InvalidUsername = 0x00002,  /*!< Invalid username was provided */
+        FileMoved = 0x00004,  /*!< File has been moved */
+        ScheduleViolation = 0x00008,  /*!< Schedule violation */
+        Authorized = 0x00010,  /*!< User has been authorized */
+        ConfigNotFound = 0x00020,  /*!< Address configuration not reconized */
+        LocationNotFound = 0x00040,  /*!< Address location not recognized */
+        DeviceNotFound = 0x00080,  /*!< Address device not recognized */
+        DeviceLocationFound = 0x00100,  /*!< Address location and device pair not recognized */
+        Challenged = 0x00200,  /*!< User is challenged */
+        Panic = 0x00400,  /*!< User has indicated a panic situation */
+        GlobalSchedViolation = 0x00800,  /*!< Global schedule violation */
+        Threat = 0x01000,  /*!< Unauthoried access threat detected */
+        Canceled = 0x02000,  /*!< User canceled access attempt */
+        LdapViolation = 0x04000,  /*!< LDAP approval violation */
+        ConfigFound = 0x08000,  /*!< Address configuration was recognized */
+        NoSingleSignOn = 0x10000  /*!< Single sign-on attempt failed */
+    }
+
+    static class SertaintyCore
+    {
+        public static string ReadString(this IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+                return "";
+            int len = 0;
+            while (Marshal.ReadByte(ptr, len) != 0)
+                len++;
+            if (len == 0)
+                return "";
+            byte[] array = new byte[len];
+
+            Marshal.Copy(ptr, array, 0, len);
+
+            return Encoding.UTF8.GetString(array);
+        }
+
+        public static byte[] ReadBytes(this IntPtr ptr, int len)
+        {
+            if (ptr == IntPtr.Zero)
+                return new byte[0];
+            if (len == 0)
+                return new byte[0];
+            byte[] array = new byte[len];
+
+            Marshal.Copy(ptr, array, 0, len);
+
+            return array;
+        }
+
+        #region uxpsys
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpsys_setLogFile(string prefix, string version);
+
+        [DllImport(SertaintyCore2.dll, CharSet = CharSet.Ansi)]
+        public static extern long uxpsys_installLicense(IntPtr error, string licenseFile);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern bool uxpsys_hasError(IntPtr handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpsys_getErrorMessage(IntPtr handle);
+
+        [DllImport(SertaintyCore2.dll, CharSet = CharSet.Ansi)]
+        public static extern long uxpsys_initLibrary(IntPtr error, long argc, string[] argv, string licenseFile, string appKey, string prefix, string version);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpsys_newCallStatusHandle();
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpsys_freeCallStatusHandle(IntPtr cs_handle);
+
+        [DllImport(SertaintyCore2.dll, CharSet = CharSet.Ansi)]
+        public static extern void uxpsys_fileReadAll(IntPtr status, string filespec, IntPtr outbuf);
+
+        #endregion
+
+        #region uxpba
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpba_newHandle();
+
+        [DllImport(SertaintyCore2.dll)]
+        //[return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern IntPtr uxpba_getData(IntPtr handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern long uxpba_getSize(IntPtr handle);
+
+        [DllImport(SertaintyCore2.dll, CharSet = CharSet.Ansi)]
+        public static extern void uxpba_setData(IntPtr handle, [MarshalAs(UnmanagedType.LPStr)]string data, long len);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpba_clearData(IntPtr handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpba_freeHandle(IntPtr handle);
+        #endregion
+
+        #region uxpfile
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpfile_newHandle();
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_openNewFile(IntPtr handle, string data, string governance, long govtype, long mods, long flags);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_addVirtualFromFile(IntPtr uxp_handle, string virName, string filespec, long pageSize, long cacheSize, long mods);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpfile_openVirtualFile(IntPtr uxp_handle, string fileSpec, Mode mode);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern long uxpfile_readVirtualFile(IntPtr uxp_handle, IntPtr vf_handle, IntPtr buffer, long mx);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_closeVirtualFile(IntPtr uxp_handle, IntPtr vf_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern bool uxpfile_compareExternalFile(IntPtr uxp_handle, string vf_name, string ext_file_name);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_close(IntPtr uxp_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_openFile(IntPtr uxp_handle, string source, Mode mode);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern AuthorizationStatus uxpfile_authenticate(IntPtr uxp_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern int uxpfile_getChallengeCount(IntPtr uxp_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern IntPtr uxpfile_getChallenge(IntPtr uxp_handle, int idx);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_addResponse(IntPtr uxp_handle, IntPtr ch_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpfile_freeHandle(IntPtr uxp_handle);
+
+
+        #endregion
+
+        #region uxpid
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpid_publishToFile(IntPtr status, string id, string doc, long mods);
+
+        #endregion
+
+        #region uxpch
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpch_freeHandle(IntPtr ch_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpch_getPrompt(IntPtr ch_handle, IntPtr ba_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpch_setValueString(IntPtr ch_handle, string value);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpch_startTimer(IntPtr ch_handle);
+
+        [DllImport(SertaintyCore2.dll)]
+        public static extern void uxpch_endTimer(IntPtr ch_handle);
+
+
+        #endregion
+
+    }
+}
